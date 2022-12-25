3bd0b0397bd5cd87d0c5635212c9046b7b1d3836
jaume vidal <jaumevibu@gmail.com>
2022-12-25 15:01:54 +0100

    Ch02 Done

---
+++ Ch02/js/index.js
@@ -0,0 +1,20 @@
+/**
+ * Toogles the state of burger menu, between open or closed
+ *
+ * @param {event} event
+ */
+function toogleBurgerMenu(event) {
+
+  if (this.classList.contains('burger--open')) {
+
+    this.classList.remove('burger--open');
+    document.querySelector('.burger__links').classList.remove('burger__links--open');
+  }
+  else {
+
+    this.classList.add('burger--open');
+    document.querySelector('.burger__links').classList.add('burger__links--open');
+  }
+}
+
+document.querySelector('.burger').addEventListener('click', toogleBurgerMenu);
