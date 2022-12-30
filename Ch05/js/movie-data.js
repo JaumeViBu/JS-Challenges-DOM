f26eb5540b08c53d8c45feb2884c582fcc0acc8c
jaume vidal <jaumevibu@gmail.com>
2022-12-30 13:11:18 +0100

    Ch05 layout done, working on responsiveness

---
+++ Ch05/js/movie-data.js
@@ -0,0 +1,45 @@
+export const movies = [
+  {
+    title: 'Avatar: The way of water',
+    image: '../media/movie-posters/avatar-way-of-water.jpg',
+  },
+  {
+    title: 'Detective Knight Rogue',
+    image: '../media/movie-posters/detective-knight-rogue.jpg',
+  },
+  {
+    title: 'Matilda: The musical',
+    image: '../media/movie-posters/matilda-the-musical.jpg',
+  },
+  {
+    title: 'Prey for the devil',
+    image: '../media/movie-posters/prey-for-the-devil.jpg',
+  },
+  {
+    title: 'Puss in boots: The last wish',
+    image: '../media/movie-posters/puss-in-boots-the-last-wish',
+  },
+  {
+    title: 'Strange world',
+    image: '../media/movie-posters/strange-world.jpg',
+  },
+  {
+    title: 'The woman king',
+    image: '../media/movie-posters/the-woman-king.jpg',
+  },
+  {
+    title: 'Violent night',
+    image: '../media/movie-posters/violent-night.jpg',
+  },
+];
+
+/**
+ * Render the given array of movie objects into the current view
+ *
+ * @export
+ * @param {[movie]} list
+ */
+export function renderMovies(list) {
+
+
+}
